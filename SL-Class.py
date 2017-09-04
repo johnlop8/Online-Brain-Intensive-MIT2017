5c32aed3a35674b7d74f51c24f6de27483662115
John Lopez <johnlop8@gmail.com>
2017-09-04 19:20:40 -0400

    Jupyter Notebook Implementation :P

--- SL-Class.py
+++ SL-Class.py
@@ -1,4 +1,3 @@
-
 from sklearn import tree #import scikit-learn library 
 # 0=Bumpy 1=Smooth
 xfeatures = [[140, 1], [130, 1], [150, 0], [170, 0]]
@@ -10,7 +9,7 @@ dtClf = tree.DecisionTreeClassifier()
 dtClf = dtClf.fit(xfeatures,ylabels)
 
 
-print dtClf.predict([[170,0]])
+print(dtClf.predict([[140,1]]))
 
 
 #Google Developers Recipe
